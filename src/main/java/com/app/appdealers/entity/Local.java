0f4254f208cbe6bc445bf461ccec6d76f625b8aa
YasserAv <yasseravalosmontero14@gmail.com>
2024-04-28 22:51:05 -0500

    Agrupacion de comercios completada
    + Se agrego algoritmo de agrupación K-means
    - Actualmente solo se crean 3 grupos, hacer que se adecue segun el numero de locales
    - Cambiar el tiempo de ejecución de la tarea
    - Falta guardar los grupos en la db

--- src/main/java/com/app/appdealers/entity/Local.java
+++ src/main/java/com/app/appdealers/entity/Local.java
@@ -1,7 +1,8 @@
 package com.app.appdealers.entity;
 
-
 import java.util.List;
+
+import org.apache.commons.math3.ml.clustering.Clusterable;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -13,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,8 +25,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Local {
-    
+public class Local implements Clusterable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -35,6 +37,9 @@ public class Local {
 
     private String telefono;
 
+    @OneToOne(mappedBy = "local")
+    private Coordenadas coordenadas;
+
     @OneToMany(mappedBy = "local", fetch = FetchType.LAZY)
     @JsonIgnore
     private List<EstadoVisita> estadoVisita;
@@ -42,5 +47,16 @@ public class Local {
     @ManyToOne
     @JoinColumn(name = "fk_grupo")
     private Grupo grupo;
+
+    @Override
+    public double[] getPoint() {
+        if (coordenadas != null) {
+            double latitud = this.coordenadas.getLatitud();
+            double longitud = this.coordenadas.getLongitud();
+            return new double[] { latitud, longitud };
+        } else {
+            throw new IllegalStateException("Coordenadas no disponibles para este local");
+        }
+    }
 
 }
--- src/main/java/com/app/appdealers/scheduled/ScheduledLocales.java
+++ src/main/java/com/app/appdealers/scheduled/ScheduledLocales.java
@@ -2,6 +2,10 @@ package com.app.appdealers.scheduled;
 
 import java.util.List;
 
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.apache.commons.math3.ml.clustering.Cluster;
+import org.apache.commons.math3.ml.clustering.KMeansPlusPlusClusterer;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,21 +19,36 @@ public class ScheduledLocales {
     @Autowired
     private LocalRepository localRepository;
 
-    @Scheduled(fixedRate = 10000) // Se ejecuta a las 0:25 am
+    @Scheduled(fixedRate = 30000) // Se ejecuta a las 0:25 am
     public void crearGruposDeLocales() {
 
         // Esta funcion lo que debe hacer es
         // 1. Seleccionar a todos los locales que no cuenten con un grupo [Atraves de un SELECT]
-        // 2. Utilizar un algoritmo para agrupar estos locales
+        // 2. Utilizar un algoritmo para agrupar estos locales en grupos de 5 estos grupos estaran formados por los comercios mas cercanos entre si
         // 3. Guardar los locales, ahora ya con un grupo, en la base de datos nuevamente
 
         List<Local> localWithoutGroup = localRepository.getAllLocalWithoutGroup();
+        
+        // Creamos un Cluster de K-Means
+        int numeroGrupos = 3;
+        // int numeroGrupos = localWithoutGroup.size() / 10;
 
-        for(Local local : localWithoutGroup) {
-            System.out.println(local.getNombre());
-        }
+        KMeansPlusPlusClusterer<Local> clusterer = new KMeansPlusPlusClusterer<>(numeroGrupos, -1, new EuclideanDistance());
+
+        List<CentroidCluster<Local>> clusters = clusterer.cluster(localWithoutGroup);
+        
+        guardarGruposEnLaDB(clusters);
 
         System.out.println("Agrupación de locales completa");
+    }
+
+    private void guardarGruposEnLaDB(List<CentroidCluster<Local>> clusters) {
+        for (int i = 0; i < clusters.size(); i++) {
+            List<Local> localesDelGrupo = clusters.get(i).getPoints();
+            for (Local local : localesDelGrupo) {
+                System.out.println(local.getId() + " " + local.getNombre() + " - Grupo " + (i + 1));
+            }
+        }
     }
 
 
