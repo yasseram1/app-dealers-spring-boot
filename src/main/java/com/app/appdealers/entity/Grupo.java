e4c51a883546f99c048be3ca37cb64c08a6fd52a
YasserAv <yasseravalosmontero14@gmail.com>
2024-04-30 00:09:41 -0500

    Dockerfile agregado

--- src/main/java/com/app/appdealers/entity/Grupo.java
+++ src/main/java/com/app/appdealers/entity/Grupo.java
@@ -2,7 +2,6 @@ package com.app.appdealers.entity;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
