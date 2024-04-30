9a6f902748d5114dff08bd4273bc2bc19c93824f
YasserAv <yasseravalosmontero14@gmail.com>
2024-04-29 23:58:31 -0500

    Arreglando ligeros bugs

--- src/main/java/com/app/appdealers/entity/Grupo.java
+++ src/main/java/com/app/appdealers/entity/Grupo.java
@@ -2,6 +2,7 @@ package com.app.appdealers.entity;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
